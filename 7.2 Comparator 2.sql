041f9f1c178d8dbadb6116a8695c86438446c1c5
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-12-02 18:29:32 -0500

    Update 7.2 Comparator 2.sql

--- 7.2 Comparator 2.sql
+++ 7.2 Comparator 2.sql
@@ -266,7 +266,7 @@ from cteEnds
 group by person_id, end_date
 ;
 
-DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id;
+DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id; -- hardcoded as 1008
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, cohort_start_date, cohort_end_date)
 select @target_cohort_id as cohort_definition_id, person_id, start_date, end_date 
 FROM #final_cohort CO
